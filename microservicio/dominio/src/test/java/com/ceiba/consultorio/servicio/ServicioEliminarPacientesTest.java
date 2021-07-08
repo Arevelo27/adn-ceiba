1b709e551ccca1dc491419b4b67e0199e4e31ed4
Andres F. Castillo <ing2013andresfe@gmail.com>
2021-07-08 08:45:46 -0500

    se realiza text de validación cuando se va a eliminar un paciente pero se encuentra realizacionado

--- microservicio/dominio/src/test/java/com/ceiba/consultorio/servicio/ServicioEliminarPacientesTest.java
+++ microservicio/dominio/src/test/java/com/ceiba/consultorio/servicio/ServicioEliminarPacientesTest.java
@@ -1,12 +1,12 @@
 package com.ceiba.consultorio.servicio;
 
 import com.ceiba.consultorio.modelo.entidad.Paciente;
-import com.ceiba.consultorio.modelo.entidad.Pago;
 import com.ceiba.consultorio.puerto.repositorio.RepositorioEntidadPaciente;
 import com.ceiba.consultorio.puerto.repositorio.RepositorioPaciente;
-import com.ceiba.consultorio.puerto.repositorio.RepositorioPago;
 import com.ceiba.consultorio.servicio.testdatabuilder.PacienteTestDataBuilder;
-import com.ceiba.consultorio.servicio.testdatabuilder.PagoTestDataBuilder;
+import com.ceiba.core.BasePrueba;
+import com.ceiba.dominio.excepcion.ExcepcionDuplicidad;
+import com.ceiba.dominio.excepcion.ExcepcionValorObligatorio;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -36,5 +36,26 @@ public class ServicioEliminarPacientesTest {
         //assert
         Long idPaciente = Long.parseLong(paciente.getIdPaciente().toString());
         assertEquals(idPaciente, ID_PACIENTE);
+    }
+
+    @Test
+    public  void validarExistenciaPreviaEntidadPacienteTest(){
+        // arrange
+        PacienteTestDataBuilder pacienteTestDataBuilder = new PacienteTestDataBuilder().conIdPaciente(22);
+
+        RepositorioPaciente repositorioPaciente = Mockito.mock(RepositorioPaciente.class);
+        RepositorioEntidadPaciente repositorioEntidadPaciente = Mockito.mock(RepositorioEntidadPaciente.class);
+        ServicioCrearPaciente servicioCrearPaciente = new ServicioCrearPaciente(repositorioPaciente);
+        ServicioEliminarPaciente servicioEliminarPaciente = new ServicioEliminarPaciente(repositorioPaciente, repositorioEntidadPaciente);
+
+        //act
+        Paciente paciente = pacienteTestDataBuilder.build();
+        Mockito.when(repositorioPaciente.crear(paciente)).thenReturn(22L);
+        servicioCrearPaciente.ejecutar(paciente);
+        Mockito.when(repositorioEntidadPaciente.existeIncluyendoIdPaciente(paciente.getIdPaciente())).thenReturn(true);
+        //servicioEliminarPaciente.ejecutar(paciente.getIdentificacion());
+
+        //assert
+        BasePrueba.assertThrows(() -> servicioEliminarPaciente.ejecutar(paciente.getIdPaciente()), ExcepcionValorObligatorio.class, servicioEliminarPaciente.EL_PACIENTE_EXISTE);
     }
 }
